f79590292eab7c747cb640bb0008baf863fea84b
unknown <unknown@example.com>
2021-12-06 18:00:16 +0800

    1.0.2

--- miniprogram/pages/invoiceList/invoiceList.js
+++ miniprogram/pages/invoiceList/invoiceList.js
@@ -5,7 +5,10 @@ Page({
      * 页面的初始数据
      */
     data: {
-        loaded: false
+      loaded: false,
+      loadPerPage: 20,
+      loadedCount:0,
+      totalCount:0,
     },
 
     /**
@@ -14,12 +17,14 @@ Page({
     onLoad: function (options) {
         const db = wx.cloud.database();
         var userOpenid = options.openid;
-        var invoiceCount = 0;
         db.collection('registeredUsers').where({
           _openid: userOpenid
         }).get().then(res=>{
-          invoiceCount = res.data[0].invoiceCount
-          this.setData({loaded : true})
+          this.setData({
+            loaded : true,
+            loadedCount : res.data[0].invoiceCount%this.data.loadPerPage,
+            totalCount : res.data[0].invoiceCount,
+          })
         })
     },
 
@@ -70,5 +75,32 @@ Page({
      */
     onShareAppMessage: function () {
 
-    }
+    },
+    // ListTouch触摸开始
+    ListTouchStart(e) {
+      this.setData({
+        ListTouchStart: e.touches[0].pageX
+      })
+    },
+    // ListTouch计算方向
+    ListTouchMove(e) {
+      this.setData({
+        ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+      })
+    },
+    // ListTouch计算滚动
+    ListTouchEnd(e) {
+      if (this.data.ListTouchDirection =='left'){
+        this.setData({
+          modalName: e.currentTarget.dataset.target
+        })
+      } else {
+        this.setData({
+          modalName: null
+        })
+      }
+      this.setData({
+        ListTouchDirection: null
+      })
+    },
 })
